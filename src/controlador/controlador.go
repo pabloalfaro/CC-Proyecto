c2d9b34c9da32a9a7abf40df21cf829b72ef55ea
pabloalfaro <pabloalfaro@correo.ugr.es>
2020-11-08 19:13:43 +0100

    Hago las primeras implementaciones en la clase controladora. #24
    Creo las estructuras de datos. Close #24.

---
+++ src/controlador/controlador.go
@@ -0,0 +1,49 @@
+package controlador
+
+import "fmt"
+
+import "github.com/pabloalfaro/Car-finder/src/anuncio"
+import "github.com/pabloalfaro/Car-finder/src/coche"
+//import "github.com/pabloalfaro/Car-finder/src/mensaje"
+import "github.com/pabloalfaro/Car-finder/src/usuario"
+
+
+//El  identificador será el propio Username
+var Usuarios map [string] usuario.Usuario
+
+var Coches [] coche.Coche
+
+var Anuncios [] anuncio.Anuncio
+
+func main(){
+	//Inicializo el map
+	Usuarios = make (map [string] usuario.Usuario)
+	
+	fmt.Println("Usuarios iniciado")
+}
+
+//Devuelve un boolean igual a True si se ha hecho la inserción y False si no se ha podido hacer
+func nuevoUsuario (u string, n string, a string, co string, ci string) bool{
+  //Compruebo si ya existe un usuario con ese username
+  _, ok := Usuarios[u]
+  
+  //Si la variable ok es igual a true entonces el username ya está registrado
+  if (ok){
+    return false
+  }else{
+    var user usuario.Usuario
+    user = usuario.NewUsuario(u, n, a, co, ci)
+    Usuarios [u] = user
+    
+    //Se ha podido hacer la inserción
+    return true
+  }
+}
+
+
+func nuevoAnuncio (u usuario.Usuario, p float32, coc coche.Coche, k int, e string, ciu string, d string, col string) {
+  var anun anuncio.Anuncio
+  
+	anun = anuncio.NewAnuncio(u, p, coc, k, e, ciu, d, col)
+	Anuncios = append(Anuncios, anun)
+}
