9843ef3c73444b4213ea0566b5f8dc2708153c2d
pabloalfaro <pabloalfaro@correo.ugr.es>
2021-01-16 18:22:09 +0100

    Añado funcionalidad relacionada con #7

--- src/controlador/controlador.go
+++ src/controlador/controlador.go
@@ -42,6 +42,107 @@ func (cont *Controlador) NuevoUsuario(u string, n string, a string, co string, c
 	}
 }
 
+
+
+//Devuelve un boolean igual a True si se ha encontrado y False si no
+func (cont *Controlador) BuscarUsuario(u string) (usuario.Usuario, bool) {
+	//Compruebo si ya existe un usuario con ese username
+	user, ok := cont.usuarios[u]
+
+	//Si la variable ok es igual a true entonces el username ya está registrado
+	if ok {
+		return user, true
+	} else {
+		return user, false
+	}
+}
+
+
+
+func (cont *Controlador) ModificarUsuario(u string, n string, a string, co string, ci string) (bool) {
+	//Compruebo si ya existe un usuario con ese username
+	user, ok := cont.usuarios[u]
+
+	//Si la variable ok es igual a true entonces el username ya está registrado
+	if ok {
+		if n != ""{
+			usuario.SetNombre(user, n)
+		}
+		if a != ""{
+			usuario.SetNombre(user, a)
+		}
+		if co != ""{
+			usuario.SetNombre(user, co)
+		}
+		if ci != ""{
+			usuario.SetNombre(user, ci)
+		}
+		return true
+	} else {
+		return false
+	}
+}
+
+
+
+func (cont *Controlador) BorrarUsuario(u string) (bool) {
+	//Compruebo si ya existe un usuario con ese username
+	_, ok := cont.usuarios[u]
+	
+	if ok {
+		delete(cont.usuarios, u)
+		return true
+	}else{
+		return false
+	}
+}
+
+
+
+func (cont *Controlador) NuevoCoche(ma string, mo string, se string, po int) bool{
+	_, r := cont.BuscarCoche(ma, mo, se, po)
+	
+	if !r {
+		var car coche.Coche
+
+		car = coche.NewCoche(ma, mo, se, po)
+		cont.coches = append(cont.coches, car)
+		return true
+	}else{
+		return false
+	}
+}
+
+
+
+func (cont *Controlador) BuscarCoche(ma string, mo string, se string, po int) (int, bool){
+	var car coche.Coche
+
+	car = coche.NewCoche(ma, mo, se, po)
+	
+	for index, i := range cont.coches {
+		if i == car{
+			return index, true
+		}
+	}
+	return -1, false
+}
+
+
+
+func (cont *Controlador) BorrarCoche(ma string, mo string, se string, po int) bool{
+	index, r := cont.BuscarCoche(ma, mo, se, po)
+	
+	if r {
+		cont.coches = append(cont.coches[index:], cont.coches[index+1:]...)
+		return true
+	}else{
+		return false
+	}
+}
+
+
+
 func (cont *Controlador) NuevoAnuncio(u string, p float32, coc coche.Coche, k int, e string, ciu string, d string, col string) bool{
 	_, ok := cont.usuarios[u]
 	
